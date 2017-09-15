f46b36d984022a40d6864d6faf029e92cd7ca714
Saromase <seite.romain@hotmail.fr>
2017-09-15 16:45:33 +0200

    Delete todo_add.php

--- todo_add.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include 'function.php';
-
-$title = $_POST['titre'];
-$object = $_POST['objet'];
-$deadline = $_POST['deadline'];
-$bdd = connectToDb();
-$reponse = $bdd->query('INSERT INTO todo(ID, titre, objet, day_add, deadline ) VALUES(NULL, '$title','$object', '2017-08-03', '$deadline');
-?>
